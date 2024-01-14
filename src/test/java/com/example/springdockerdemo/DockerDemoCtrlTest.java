15a1ca77e0fab3674e0cdbe9e2e40c001c6e76a0
SWAPNIL <swapnil14392@gmail.com>
2024-01-14 15:13:36 +0530

    docker image with junit config

---
+++ src/test/java/com/example/springdockerdemo/DockerDemoCtrlTest.java
@@ -0,0 +1,20 @@
+package com.example.springdockerdemo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class DockerDemoCtrlTest {
+    /**
+     * Method under test: {@link DockerDemoCtrl#display()}
+     */
+    @Test
+    void testDisplay() {
+        //   Diffblue Cover was unable to write a Spring test,
+        //   so wrote a non-Spring test instead.
+        //   Diffblue AI was unable to find a test
+
+        assertEquals("welcome to docker", (new DockerDemoCtrl()).display());
+    }
+}
+
